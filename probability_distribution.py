0902e24148d612cbf2cd7532f97066a91bc063c9
agarwalharsh2156 <130427016+agarwalharsh2156@users.noreply.github.com>
2024-05-14 15:53:36 +0530

    modification made in explanation of the probability distribution

--- probability_distribution.py
+++ probability_distribution.py
@@ -1,5 +1,5 @@
 #---------------------------------------------------------------------------------------
-# import of necessary libraries
+# importing necessary libraries
 
 import numpy as np
 # import scipy.stats for using various probability distribution methods such as binom, poisson and norm
@@ -10,11 +10,16 @@ import matplotlib.pyplot as plt
 #---------------------------------------------------------------------------------------
 
 # Binomial Distribution by Bernoulli
+# EXPLANATION>>>
+# Binomial Distribution is a distribution of probabilities where we are concerned with 2 outcomes only
+# i.e. success and failure. Here, given success constant in all experiments, we try to find probability of success in n trials.
 
+# Question>>>
 # A bank issues credit cards under the scheme of Master Card. Based on the past data,
 # the bank has found out that 60% of all accounts pay their bills on time. If a sample of
 # 7 accounts are selected from the current database, construct the binomial distribution of acounts paying on time.
 
+#SOLUTION>>>
 #---------------------------------------------------------------------------------------
 
 n= 7 # no. of records selected to be found probability on
@@ -32,3 +37,12 @@ plt.ylabel("Probability of succcess")
 plt.show()
 
 #---------------------------------------------------------------------------------------
+
+# Poisson DIstribution by Poisson 
+# EXPLANATION>>>
+# It is a probability distribution similar to Binomial DIstribution. Only, it doesn't concern about number of 
+# trials probability of success to be found. Rather, it calculates, given probability of success constant in all the outcomes of
+# experiment, probability of success over a given unit of outcomes.
+
+#Question>>>
+
