20cb7521cd018e80db2f1c8fccad5949a6426a5c
agarwalharsh2156 <130427016+agarwalharsh2156@users.noreply.github.com>
2024-05-15 20:54:01 +0530

    pandas_practice.py was modified

--- pandas_practice.py
+++ pandas_practice.py
@@ -7,22 +7,30 @@ dictionary = {"Gender": ["Male", "Female"], "Age": [21, 20], "Occupation": ["IT 
 df = pd.DataFrame(dictionary , index=["Cindy", "Chinki"])
 print(df,"\n")
 
+
 # using an attribute with the object dataframe created and passing the index for the value we want to retrieve
 # attribute can simply be assumed as the column name
 # this technique returns a cell in a particular attribute at a particular row index
 print(df.Age["Chinki"],"\n")
 
+
 # Series : Each column of a dataFrame is known as Series
 # You can access a column(Series) using the code example shown below
 print(df["Occupation"],"\n")
+
 
 # .max() can be used over a particular series to know the maximum among the entries in the table
 # .min() can be used to know the minimum
 print(df["Age"].max())
 print(df["Age"].min(),"\n")
 
+
 # .describe() outputs the basic statistical interpretation of the numeic data
 print(df["Age"].describe(),"\n")
 
-# .groupby() groups the data based on a particular series
+
+# .groupby() groups the data based on a particular series and provides the statistical interpretation with respect to it
+# used to find relation between two series or trend in one series with respect to other
 print(df.groupby("Age")["Gender"].describe())
+# In the above code we are looking for trend in Female with respect to Age. Also here the interpretation is not too informative,
+# due to the size of dataset.
