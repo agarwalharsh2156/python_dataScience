b71601f0651a4b0503c32a295539d8eded4f10e0
agarwalharsh2156 <130427016+agarwalharsh2156@users.noreply.github.com>
2024-05-15 19:16:54 +0530

    pandas_practice.py is created

---
+++ pandas_practice.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({"Cindy": ["Male", 20, "IT Student"], "Chinki": ["Female", 20, "Python Enthusiast"]}, index=["Gender", "Age", "Occupation"])
+print(df)
